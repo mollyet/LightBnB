560dcfc0929707d3a5fd9437d4ebc968c272f028
Molly Trepanier <mtrepanier123@gmail.com>
2021-03-04 06:11:50 +0000

    inital commit, created lightbnb database and initial tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR (255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE properties (
+id SERIAL PRIMARY KEY,
+owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+title VARCHAR(255) NOT NULL, 
+description TEXT,
+thumbnail_photo_url VARCHAR(255) NOT NULL,
+cover_photo_url VARCHAR(255) NOT NULL,
+cost_per_night SMALLINT NOT NULL DEFAULT 0,
+number_of_beds SMALLINT NOT NULL DEFAULT 0,
+number_of_bathrooms SMALLINT NOT NULL DEFAULT 0, 
+parking_spaces SMALLINT NOT NULL DEFAULT 0,
+
+country VARCHAR(255) NOT NULL,
+province VARCHAR(255) NOT NULL,
+city VARCHAR(255) NOT NULL,
+street_address VARCHAR(255) NOT NULL,
+postal_code VARCHAR(255) NOT NULL,
+
+active BOOLEAN NOT NULL DEFAULT true
+);
+
+CREATE TABLE reservations (
+id SERIAL PRIMARY KEY NOT NULL,
+start_date DATE NOT NULL,
+end_date DATE NOT NULL,
+guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  message TEXT,
+  rating SMALLINT NOT NULL DEFAULT 0
+);
